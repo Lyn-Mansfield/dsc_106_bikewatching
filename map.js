79cb5ebe38ab552781e50448f245afd32e96be51
Lyn Mansfield <imansfield@ucsd.edu>
2026-05-11 14:28:44 -0700

    added biking data

--- map.js
+++ map.js
@@ -1,11 +1,13 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
 // Set your Mapbox access token here
-mapboxgl.accessToken = 'pk.eyJ1IjoibHluLW1hbnNmaWVsZCIsImEiOiJjbXAxb2t5NTAwN2t2MnVvYW1tNTNkMjVlIn0.40k7nOKa_SjOcBuS1QJ6ww';
+mapboxgl.accessToken = "pk.eyJ1IjoibHluLW1hbnNmaWVsZCIsImEiOiJjbXAxb2t5NTAwN2t2MnVvYW1tNTNkMjVlIn0.40k7nOKa_SjOcBuS1QJ6ww";
 
 // Initialize the map
 const map = new mapboxgl.Map({
@@ -20,7 +22,7 @@ const map = new mapboxgl.Map({
 map.on('load', async () => {
     map.addSource('boston_route', {
         type: 'geojson',
-        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+        data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson",
     });
 
     map.addLayer({
@@ -33,4 +35,19 @@ map.on('load', async () => {
             'line-opacity': 0.6       // Slightly less transparent
         }
     });
+
+    let jsonData;
+    try {
+        const jsonurl = "https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv";
+
+        // Await JSON fetch
+        const jsonData = await d3.json(jsonurl);
+
+        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+    } catch (error) {
+        console.error('Error loading JSON:', error); // Handle errors
+    }
+
+    let stations = jsonData.data.stations;
+    console.log('Stations Array:', stations);
 });
